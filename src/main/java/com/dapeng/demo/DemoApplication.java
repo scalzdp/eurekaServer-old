c866e45383133b8ef1e4c9079bf47626b0a068d0
007956@sichuanair.com <007956@sichuanair.com>
2019-04-12 17:42:24 +0800

    Initial commit

---
+++ src/main/java/com/dapeng/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.dapeng.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+
+}
