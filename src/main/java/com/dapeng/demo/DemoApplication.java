61197c8196bdff1abc4298006c7744e8c284cd4a
scalzdp <275735763@qq.com>
2019-04-14 15:58:02 +0800

    修改注册方式的配置

--- src/main/java/com/dapeng/demo/DemoApplication.java
+++ src/main/java/com/dapeng/demo/DemoApplication.java
@@ -3,9 +3,9 @@ package com.dapeng.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -15,13 +15,13 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @EnableWebSecurity
-    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-            super.configure(http);
-        }
-    }
+//    @EnableWebSecurity
+//    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.csrf().disable();
+//            super.configure(http);
+//        }
+//    }
 
 }
